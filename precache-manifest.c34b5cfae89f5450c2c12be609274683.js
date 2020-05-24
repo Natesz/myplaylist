6fb9cecbf784d991e826788ca4b82fe514c9d986
Natesz <natesz777@yahoo.com>
2020-05-24 18:35:36 +0200

    Updates

---
+++ precache-manifest.c34b5cfae89f5450c2c12be609274683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951ff7d164186e2097d20ef8444c0cb1",
+    "url": "/myplaylist/index.html"
+  },
+  {
+    "revision": "a25f8b278f87b3c353e4",
+    "url": "/myplaylist/static/css/main.82946c7c.chunk.css"
+  },
+  {
+    "revision": "ebc77c1a04e86eb81e6c",
+    "url": "/myplaylist/static/js/2.e8959b62.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/myplaylist/static/js/2.e8959b62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a25f8b278f87b3c353e4",
+    "url": "/myplaylist/static/js/main.965e5374.chunk.js"
+  },
+  {
+    "revision": "4fa1dd486f2fd0e421e9",
+    "url": "/myplaylist/static/js/runtime-main.faf66327.js"
+  }
+]);
